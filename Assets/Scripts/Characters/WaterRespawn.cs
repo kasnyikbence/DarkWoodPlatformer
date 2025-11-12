ed1a424ad606e249d25ee42ec0ae1e68f222af07
Kasnyik Bence <h263908@stud.u-szeged.hu>
2025-11-12 12:16:33 +0100

    tutorial pálya szerkezetének befejezése

--- Assets/Scripts/Characters/WaterRespawn.cs
+++ Assets/Scripts/Characters/WaterRespawn.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 public class WaterRespawn : MonoBehaviour
 {
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private int damageAmount = 5;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.CompareTag("Player"))
         {
-            collision.transform.position = respawnPoint.position;
-
             Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            Damageable damageable = collision.GetComponent<Damageable>();
+
+
+            if (damageable != null)
+            {
+                damageable.Hit(damageAmount, Vector2.zero);
+            }
+
+            collision.transform.position = respawnPoint.position;
+            
             if (rb != null)
             {
                 rb.linearVelocity = Vector2.zero;
